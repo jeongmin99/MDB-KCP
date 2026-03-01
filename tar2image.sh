b2f7c05cc2141ddc21d3dc39d84166f755ac7f5b
jeongmin99 <jmlee990909@gmail.com>
2026-03-01 18:14:29 +0900

    refactor: 폴더 구조화

--- tar2image.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-newcontainer=$(buildah from scratch)
-path=$1
-buildah add $newcontainer $path /
-buildah config --annotation=io.kubernetes.cri-o.annotations.checkpoint.name=redis-snpashot $newcontainer
-buildah commit $newcontainer redis-snapshot:latest
-buildah rm $newcontainer
